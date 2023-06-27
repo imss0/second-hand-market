583e6883fb7a6fbbfdc5b4d3d5439dfdf10d8790
sohyun <shlim0410@gmail.com>
2023-06-27 16:10:14 +0100

    feat: create product card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import CheckoutPriceCard from "./components/CheckoutPriceCard";
+import ProductCard from "./components/ProductCard";
 import ProfileCard from "./components/ProfileCard";
 
 export default function Home() {
   return (
-    <main className="bg-slate-300 py-20 px-10 grid gap-10">
+    <main className="bg-slate-300 py-20 px-10 grid gap-10 min-h-screen">
       <CheckoutPriceCard />
       <ProfileCard />
+      <ProductCard />
     </main>
   );
 }
